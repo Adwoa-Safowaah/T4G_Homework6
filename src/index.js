e60291775705757b8564c352d8bac64b72845335
Adwoa-Safowaah <adwoaadysne@gmail.com>
2024-12-02 22:39:40 +0000

    Added my files and also added some data to my javascript

---
+++ src/index.js
@@ -0,0 +1,29 @@
+let myName = "Adwoa Safowaah";
+console.log(myName); 
+
+let myAge = "27";
+console.log(myAge);
+
+let sum = 10 + 15;
+console.log(sum);
+
+let difference = 30 - 12;
+console.log(difference);
+
+let division = 50 / 5;
+console.log(division);
+
+let product = 6 * 7;
+console.log(product);
+
+let modulo = 15 % 4;
+console.log(modulo);
+
+let isStudent = true;
+console.log(isStudent);
+
+let someVariable = 42;
+console.log(typeof someVariable);
+
+someVariable = "Hello World";
+console.log(typeof someVariable);
